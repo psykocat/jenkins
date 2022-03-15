a5cdc9f6024c9cfc9bcc9bb020654f1c9a090d84
PsykoCat <PsykoCat at nomail dot localhost>
2022-03-15 18:43:49 +0100

    Update build script

--- tools/build.sh
+++ tools/build.sh
@@ -6,15 +6,17 @@ _env_file=${JENKINS_ENV_FILE:-.env}
 
 # Docker / Dockerfile won't handle symlink following so we cp the wanted files to create a jenkins template repository
 jks_tpl=templates/jenkins
+files_to_remove=()
+files_to_remove+=("${jks_tpl}")
+do_push=
 
 function cleanup(){
-	rm -rf -- "${jks_tpl}"
+	rm -vrf -- "${files_to_remove[@]}"
 }
 trap cleanup EXIT
 
 cleanup
 
-do_push=
 while [ $# -ne 0 ]; do
 	case $1 in
 		--debug)
@@ -44,30 +46,30 @@ if [ -s "${_env_file}" ]; then
 	. "${_env_file}"
 	set +a
 fi
+_jks_custom_cfg="jenkins.config.env"
 
 # Copy file list for jenkins template
-for jfile in jenkins.config.env ${DOCKERFILE} docker-compose*.yml run.sh README.md "${_env_file}" .gitignore .editorconfig Jenkinsfile.disabled global_shared_libraries.json.example; do
+for jfile in "${_jks_custom_cfg}" "${DOCKERFILE}" docker-compose*.yml run.sh README.md "${_env_file}" .gitignore .editorconfig Jenkinsfile.disabled global_shared_libraries.json.example; do
     # Try first with a tmpl if it exists, otherwise the default file
-    for jext in ".tmpl" ""; do
-        base=${jfile}${jext}
-        dest=${base%.tmpl}
-        mkdir -p $(dirname ${jks_tpl}/${base})
-        if [ -f "${base}" ]; then
-            cp -vL ${base} ${jks_tpl}/${dest}
-            break
-        fi
-    done
+	_jext=".tmpl"
+	_base="${jfile}"
+	_dest="${jfile}"
+	if [ -s "${jfile}${_jext}" ]; then
+		_base="${jfile}${_jext}"
+	fi
+	mkdir -p $(dirname "${jks_tpl}/${_base}")
+	if [ -s "${_base}" ]; then
+		cp -vL "${_base}" "${jks_tpl}/${_dest}"
+	fi
 done
 
-export WORKSPACE=${PWD}
+image_tag="${DOCKER_REGISTRY:+${DOCKER_REGISTRY}/}jenkins:${VERSION}"
+image_latest="${image_tag%:*}:latest"
 
-docker-compose -f docker-compose.yml -f docker-compose.local.yml build --force-rm --no-cache --pull jenkins
-tag_filename="${jks_tpl}/jenkins_release_tagname"
-docker-compose -f docker-compose.yml -f docker-compose.local.yml config | yq -r '.services.jenkins.image' > "${tag_filename}"
-tag_contents=$(cat "${tag_filename}")
-docker tag "${tag_contents}" "${tag_contents%:*}:latest"
+docker build -t "${image_tag}" -t "${image_latest}" -f "${DOCKERFILE}" .
 
 if [ "${do_push}" = "yes" ]; then
-	docker-compose -f docker-compose.yml -f docker-compose.local.yml push -- jenkins
+	docker push "${image_tag}"
+	docker push "${image_latest}"
 fi
 #END
