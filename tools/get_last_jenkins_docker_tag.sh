8adb87e80dd1018f360adb7ce8fab0c12495c834
PsykoCat <PsykoCat at nomail dot localhost>
2022-09-30 12:01:08 +0200

    Update fetch script to handle new version of hub api

--- tools/get_last_jenkins_docker_tag.sh
+++ tools/get_last_jenkins_docker_tag.sh
@@ -3,7 +3,7 @@
 set -eu
 
 image_name=$(awk '/^FROM/{ sub(":.*", ""); print $2;}' Dockerfile)
-tags_url="https://registry.hub.docker.com/v1/repositories/${image_name}/tags"
+tags_url="https://registry.hub.docker.com/v2/repositories/${image_name}/tags?page=1&page_size=150"
 last_tag_match="[0-9].*-alpine$"
 last_lts_match="[0-9].*lts-alpine$"
 verbose=
@@ -24,7 +24,7 @@ done
 
 jq_var="last_${matching_element}_match"
 
-last_tag=$(curl -sSL "${tags_url}"|jq --arg match_repr "${!jq_var}" -r '.[]|select(.name|match($match_repr))|.name'|sort -V|tail -1)
+last_tag=$(curl -sSL "${tags_url}"|jq --arg match_repr "${!jq_var}" -r '.results[]|select(.name|match($match_repr))|.name'|sort -V|tail -1)
 
 if [ "${verbose}" = "yes" ]; then
     echo "Last tag for ${image_name} is '${last_tag}'"
